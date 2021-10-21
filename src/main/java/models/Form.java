a91b29b6b7880e5706d4d29a88597f5124758e2f
StativaGeorgiana <stativa50@gmail.com>
2021-10-22 00:10:04 +0300

    Finished form models(Form + Form Question)

--- src/main/java/models/Form.java
+++ src/main/java/models/Form.java
@@ -1,4 +1,36 @@
 package models;
 
+/**
+ * A class that contains the structure and information af a Form
+ */
 public class Form {
+
+    private String id;
+
+    /**
+     * The id of the post the form is related with
+     */
+    private String postId;
+
+
+    public String getPostId() {
+
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+
+        this.postId = postId;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
 }
--- src/main/java/models/FormQuestion.java
+++ src/main/java/models/FormQuestion.java
@@ -1,4 +1,57 @@
 package models;
 
+/**
+ * A class that contains the structure and information af a Form
+ */
 public class FormQuestion {
+
+    private String id;
+
+    /**
+     * The id of the form that contains this question
+     */
+    private String formId;
+
+    /**
+     * The question content
+     */
+    private String content;
+
+    private FormQuestion(){
+
+    }
+
+    public static class Builder{
+
+        private final FormQuestion formQuestionInstance;
+
+        public Builder(){
+
+            formQuestionInstance = new FormQuestion();
+        }
+
+        public Builder withId(String id){
+
+            formQuestionInstance.id = id;
+            return this;
+        }
+
+        public Builder withFormId(String formId){
+
+            formQuestionInstance.formId = formId;
+            return this;
+        }
+
+        public Builder withContent(String content){
+
+            formQuestionInstance.content = content;
+            return this;
+        }
+
+        public FormQuestion build(){
+
+            return formQuestionInstance;
+        }
+    }
+
 }
