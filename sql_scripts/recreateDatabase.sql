c9d62d8060582be2ad0ac7cdeca962304f3482e9
StativaGeorgiana <stativa50@gmail.com>
2021-10-30 21:07:15 +0300

    queries for form

--- sql_scripts/recreateDatabase.sql
+++ sql_scripts/recreateDatabase.sql
@@ -1,29 +1,67 @@
 -- Delete current database
 -- MAKE SURE YOU DON'T DELETE SOMETHING YOU NEED
-DROP DATABASE lostandfound;
+DROP
+DATABASE lostandfound;
 
 -- Create database
-CREATE DATABASE lostandfound;
+CREATE
+DATABASE lostandfound;
 
 -- Select database
-USE lostandfound;
+USE
+lostandfound;
 
 -- Create posts table
-CREATE TABLE posts(
-	id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT(6) NOT NULL,
-    status ENUM("PENDING_LOST", "ACTIVE_LOST", "PENDING_FOUND", "ACTIVE_FOUND", "SOLVED", "CLOSED"),
+CREATE TABLE posts
+(
+    id       INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id  INT(6) NOT NULL,
+    status   ENUM("PENDING_LOST", "ACTIVE_LOST", "PENDING_FOUND", "ACTIVE_FOUND", "SOLVED", "CLOSED"),
     has_form BOOLEAN NOT NULL DEFAULT 0
 );
 
 -- Create postsdetails table
 
-CREATE TABLE postsdetails(
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    post_id INT(6) UNSIGNED NOT NULL,
-    title VARCHAR(50) NOT NULL,
-    category ENUM("WALLET"),
+CREATE TABLE postsdetails
+(
+    id          INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    post_id     INT(6) UNSIGNED NOT NULL,
+    title       VARCHAR(50) NOT NULL,
+    category    ENUM("WALLET"),
     description VARCHAR(10000),
-    created_at timestamp default current_timestamp,
-    FOREIGN KEY(post_id) REFERENCES posts(id)
+    created_at  timestamp default current_timestamp,
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+
+-- Create form table
+
+CREATE TABLE forms
+(
+    id      INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    post_id INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (post_id)
+        REFERENCES posts (id)
+        ON DELETE CASCADE
+);
+
+-- Create form question table
+CREATE TABLE formquestions
+(
+    id      INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    form_id INT(6) UNSIGNED NOT NULL,
+    content VARCHAR(2000),
+    FOREIGN KEY (form_id)
+        REFERENCES forms (id)
+        ON DELETE CASCADE
+);
+
+-- Create form answer table
+CREATE TABLE formanswer
+(
+    id          INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    question_id INT(6) UNSIGNED NOT NULL,
+    user_id     INT(6) UNSIGNED NOT NULL,
+    FOREIGN KEY (question_id)
+        REFERENCES formquestions (id)
+        ON DELETE CASCADE
 );
