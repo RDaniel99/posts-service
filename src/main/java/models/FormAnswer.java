330af081fcda0e7fd56053639567d062f0772976
StativaGeorgiana <stativa50@gmail.com>
2021-10-30 19:33:39 +0300

    FormAnswer Model + Repo

---
+++ src/main/java/models/FormAnswer.java
@@ -0,0 +1,59 @@
+package models;
+
+/**
+ * A class that represents the structure and information for a form answer
+ */
+public class FormAnswer {
+
+    /**
+     * The question this answer is associated with
+     */
+    private String questionId;
+
+    /**
+     * The user that answered to the question
+     */
+    private String userId;
+
+    /**
+     * The question content
+     */
+    private String content;
+
+    private FormAnswer() {
+
+    }
+
+    public class Builder {
+
+        private FormAnswer formAnswerInstance;
+
+        public Builder() {
+
+        }
+
+        public Builder withQuestionId(String questionId) {
+
+            formAnswerInstance.questionId = questionId;
+            return this;
+        }
+
+        public Builder withUserId(String userId) {
+
+            formAnswerInstance.userId = userId;
+            return this;
+        }
+
+        public Builder withContent(String content) {
+
+            formAnswerInstance.content = content;
+            return this;
+        }
+
+        public FormAnswer build() {
+
+            return formAnswerInstance;
+        }
+    }
+
+}
